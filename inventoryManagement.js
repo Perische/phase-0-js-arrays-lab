ae2577ea19ddc9ba6d0daca177f35d23252784e2
Peris Wamweu <peris.wamweu@student.moringaschhol.com>
2026-06-19 13:15:15 +0300

    added array functions to the inventory

--- inventoryManagement.js
+++ inventoryManagement.js
@@ -1,4 +1,40 @@
-// Write your code here
+// W// Create an array of products
+let products = ["Laptop", "Phone", "Headphones", "Monitor"];
+
+// Function to log the first product
+function logFirstProduct() {
+  console.log(products[0]);
+}
+
+// Function to add a new product
+function addProduct(productName) {
+  products.push(productName);
+  console.log(`${productName} added to inventory.`);
+}
+
+// Function to update a product name by position (index)
+function updateProductName(index, newName) {
+  if (index >= 0 && index < products.length) {
+    products[index] = newName;
+    console.log(`Product at position ${index} updated to ${newName}.`);
+  } else {
+    console.log("Invalid product index.");
+  }
+}
+
+// Function to remove the last product
+function removeLastProduct() {
+  let removedProduct = products.pop();
+  console.log(`${removedProduct} removed from inventory.`);
+}
+
+// Example usage:
+logFirstProduct();
+addProduct("Tablet");
+updateProductName(1, "Smartphone");
+removeLastProduct();
+
+console.log("Final products list:", products);
 
 
 
